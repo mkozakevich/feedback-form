8edd9844c83d497c95bf5e4760bc796b4ca6e928
Maksim Kozakevich <kotseee@yandex.ru>
2024-05-27 01:47:40 +0300

    feat: add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,32 @@
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-IuXqUzWo9N8kVGArXWlyrfmSVUkFkLY',
+  authDomain: 'feedback-form-2c525.firebaseapp.com',
+  projectId: 'feedback-form-2c525',
+  storageBucket: 'feedback-form-2c525.appspot.com',
+  messagingSenderId: '292648262485',
+  appId: '1:292648262485:web:d3ee2ce38fcaf1054caddc',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(TuiRootModule)]
+  providers: [
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(TuiRootModule),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.less'
+  styleUrl: './chart.component.less',
 })
 export class ChartComponent {
-
+  private readonly firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+  
+  constructor() {
+    const feedbackCollection = collection(this.firestore, 'feedback');
+    this.items$ = collectionData(feedbackCollection);
+  }
 }
